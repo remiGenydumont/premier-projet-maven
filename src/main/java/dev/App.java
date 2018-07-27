a275ad7c748aafa5c5fe0fe4192917a57eada4c2
Remi Geny-Dumont <remi.genydumont@gmail.com>
2018-07-27 15:32:02 +0200

    adding profil to pom.xml

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -8,10 +10,16 @@ import com.github.lalyos.jfiglet.FigletFont;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	System.out.println(FigletFont.convertOneLine("APP"));
-    	String name = "Rémi";
-        System.out.println( "Hello "+name+"!");
-    }
+	public static void main( String[] args )
+	{
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+
+		String name = "Rémi";
+		System.out.println( "Hello "+name+"!");
+	}
 }
