9b7c52edf4df37781d254d0bce8c26ff840bfabf
Remi Geny-Dumont <remi.genydumont@gmail.com>
2018-07-27 11:15:52 +0200

    updating pom.xml

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String name = "Rémi";
+        System.out.println( "Hello "+name+"!");
     }
 }
